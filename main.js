b1c28d7cd63a7814f8bd0ed24329316313d35a51
Robert Januszewski <easiv89@gmail.com>
2019-01-28 20:57:46 +0100

    basic exercise updating

--- main.js
+++ main.js
@@ -6,8 +6,22 @@ var app = new Vue({
     },
     methods: {
       addExercise() {
-        this.exerciseList.push({name: this.newExercise});
+        this.exerciseList.push({id: this.setId(), name: this.newExercise});
         this.newExercise = ''
+      },
+
+      updateExercise(id) {
+        this.exerciseList[id].name = 'updated';
+      },
+
+      setId() {
+        const exerciseLength = this.exerciseList.length;
+
+        if(exerciseLength === 0) {
+          return 0;
+        } else {
+          return this.exerciseList[exerciseLength - 1].id + 1;
+        }
       }
     }
   })
