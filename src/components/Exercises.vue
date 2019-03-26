e43a436e50678c4b477b83124ec6f8740ed3798b
Easiv <easiv89@gmail.com>
2019-03-26 22:26:06 +0100

    done button rework, styling

--- src/components/Exercises.vue
+++ src/components/Exercises.vue
@@ -9,32 +9,42 @@
         ></v-autocomplete>
       </v-card-text>
 
-      <v-layout row align-center v-if="exerciseExists(newExercise)">
-        <v-flex xs12 sm6 md3>
-          <v-text-field
-            v-model="exerciseProps.quantity"
-            label="Quantity"
-            solo
-          ></v-text-field>
-        </v-flex>
-
-      <v-flex xs12 sm6 md3>
-        <v-text-field
-          v-model="exerciseProps.rating"
-          label="Rating"
-          solo
-        ></v-text-field>
-        </v-flex>
-        <v-layout justify-end>
-          <v-icon large @click="addExercise">done_outline</v-icon>
-        </v-layout>
-      </v-layout>
+      <v-card v-if="newExercise">
+        <v-card-title>
+          <h1>Series</h1>
+          <v-layout row justify-center>
+            <v-flex xs12 sm6 md3>
+              <v-text-field
+                v-model="newSeries.repetitions"
+                label="Repetitions"
+                solo
+              ></v-text-field>
+            </v-flex>
+            <v-flex xs12 sm6 md3>
+              <v-text-field
+                v-model="newSeries.weight"
+                label="Weight"
+                solo
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout column align-end>
+            <v-icon @click="addSeries">add</v-icon>
+            <v-icon @click="addExercise" large  color="green">done</v-icon>
+          </v-layout>
+        </v-card-title>
+      </v-card>
     </v-card>
     <br />
 
     <span v-for="(exercise, index) in exerciseList" v-bind:key="exercise.id">
       <v-card v-if="exercise">
         <v-card-title primary-title>
+          <span v-for="(series, idx) in exercise.series" v-bind:key="series.repetitions">
+          <v-card>
+            {{idx}} {{series.repetitions}} {{series.weight}} {{series.time}}
+          </v-card>
+          </span>
           <h3 class="headline mb-0" @click="showExerciseEdit(index)">{{exercise.name}}</h3>
           <v-layout column justify-end align-end v-if="!exercise.editing">
             <h4 v-if="exercise.quantity">Quantity: {{exercise.quantity}}</h4>
@@ -71,6 +81,12 @@ export default {
   data() {
     return {
       newExercise: '',
+      newSeries: {
+        repetitions: '',
+        weight: '',
+        time: Date.now()
+      },
+      newSeriesArray: [],
       exerciseProps: {
         quantity: '',
         rating: ''
@@ -91,17 +107,20 @@ export default {
           list.push({id: 0,
                     name: this.newExercise,
                     editing: false,
+                    series: this.newSeriesArray,
                     quantity: this.exerciseProps.quantity,
                     rating: this.exerciseProps.rating });
         } else {
           list.push({id: this.setId(),
                     name: this.newExercise,
                     editing: false,
+                    series: this.newSeriesArray,
                     quantity: this.exerciseProps.quantity,
                     rating: this.exerciseProps.rating });
         }
       }
       this.newExercise = ''
+      this.newSeriesArray = [];
       this.exerciseProps = { quantity: '', rating: '' }
     },
     deleteExercise(index) {
@@ -117,8 +136,19 @@ export default {
     setId() {
       return this.exerciseList[this.exerciseList.length - 1].id + 1;
     },
+    setSeriesId() {
+      return this.newSeriesArray[this.newSeriesArray.length - 1].id + 1;
+    },
     exerciseExists(item) {
       return this.availableExercises.includes(item);
+    },
+    addSeries() {
+      this.newSeriesArray.push(this.newSeries);
+      this.newSeries = {
+        repetitions: '',
+        weight: '',
+        time: Date.now()
+      }
     }
   }
 }
